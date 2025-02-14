89a4cadc49915a98f3bb7110237cd9c48f27dfd4
FridayerWang <FridayerWang@gmail.com>
2025-02-14 05:45:32 -0500

    Complete Task 1

--- Java/src/main/java/frogger/Frogger.java
+++ Java/src/main/java/frogger/Frogger.java
@@ -36,24 +36,27 @@ public class Frogger {
      */
     public boolean move(boolean forward) {
         int nextPosition = this.position + (forward ? 1 : -1);
-        if (!isValid(nextPosition) || isOccupied(nextPosition)) {
+//        if (!isValid(nextPosition) || isOccupied(nextPosition)) {
+//            return false;
+//        }
+        if (!this.road.isValid(nextPosition) || this.road.isOccupied(nextPosition)) {
             return false;
         }
         this.position = nextPosition;
         return true;
     }
 
-    // TODO: Do you notice any issues here?
-    public boolean isOccupied(int position) {
-        boolean[] occupied = this.road.getOccupied();
-        return occupied[position];
-    }
-    
-    public boolean isValid(int position) {
-        if (position < 0) return false;
-        boolean[] occupied = this.road.getOccupied();
-        return position < occupied.length;
-    }
+//    // TODO: Do you notice any issues here?
+//    public boolean isOccupied(int position) {
+//        boolean[] occupied = this.road.getOccupied();
+//        return occupied[position];
+//    }
+//
+//    public boolean isValid(int position) {
+//        if (position < 0) return false;
+//        boolean[] occupied = this.road.getOccupied();
+//        return position < occupied.length;
+//    }
 
     /**
      * Records Frogger to the list of records.
